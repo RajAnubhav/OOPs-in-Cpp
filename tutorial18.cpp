d69def147de3c08ed3301c5c558ad5416497f33c
Anubhav <93657903+RajAnubhav@users.noreply.github.com>
2022-03-02 12:50:59 +0530

    Function Overloading
    C++ allows the specification of more than one function of the same name in the same scope. These functions are called overloaded functions

--- tutorial18.cpp
+++ tutorial18.cpp
@@ -1,16 +1,24 @@
-// function overloading
+// this is the program of function overloading in cpp
 #include<iostream>
 using namespace std;
+
+// this is the function having 2 parameters
 int sum(int a, int b){
     cout<<"Using function with 2 arguments"<<endl;
     return a+b;
 }
+
+// this is the function having 3 parameters
 int sum(int a, int b, int c){
     cout<<"Using function with 3 arguments"<<endl;
     return a+b+c;
 }
+
 int main(){
-    cout<<"The sum of 3 and 6 is "<<sum(3,6)<<endl;
-    cout<<"The sum of 3, 7 and 6 is "<<sum(3,7,6)<<endl;
+    // this will call the sum() with 2 parameters
+    cout<<"The sum of 3 and 6 is: "<<sum(3, 6)<<endl;
+    
+    // this will call the sum() with 3 parameters
+    cout<<"The sum of 3, 7 and 6 is "<<sum(3, 7, 6)<<endl;
     return 0;
 }
