fa3e43a7bf614c276059f3ccb65167f33e2dd419
Anubhav <93657903+RajAnubhav@users.noreply.github.com>
2022-03-03 06:53:11 +0530

    Default and Parameterized Constructor
    Default Constructors: The default constructor is the constructor which doesn’t take any argument. It has no parameters.
    
    Parameterized Constructors: It is possible to pass arguments to constructors. Typically, these arguments help initialize an object when it is created. To create a parameterized constructor, simply add parameters to it the way you would to any other function. When you define the constructor’s body, use the parameters to initialize the object.

--- tutorial30.cpp
+++ tutorial30.cpp
@@ -1,4 +1,19 @@
-// Paramatized and Default Constructor
+// There are three types of constructors
+// 1. Default constructors
+// 2. Parameterized constructors
+// 3. Copy constructors
+
+// here we will be discussing only Paramatized and Default Constructors
+
+// 1. Default Constructors: Default constructor is the constructor which 
+// doesn’t take any argument. It has no parameters.
+
+// 2. Parameterized Constructors: It is possible to pass arguments to constructors. 
+// Typically, these arguments help initialize an object when it is created. 
+// To create a parameterized constructor, simply add parameters to it the way 
+// you would to any other function. When you define the constructor’s body, 
+// use the parameters to initialize the object. 
+
 #include <iostream>
 using namespace std;
 
@@ -8,13 +23,13 @@ class Complex
 
 public:
     //Creating a Constructor
-    Complex(int, int);
+    Complex(int, int); // declaration of the constructor 
     void printNumber()
     {
         cout << "Your number is " << a << " + " << b << "i " << endl;
     }
 };
-Complex ::Complex(int x, int y)
+Complex ::Complex(int x, int y) // this is parameterized constructer
 {
     a = x;
     b = y;
@@ -32,3 +47,10 @@ int main()
     b.printNumber();
     return 0;
 }
+
+
+// <<<<<<<----------   output   ------------->>>>>>>>>>>
+/*
+Your number is 4 + 6i 
+Your number is 5 + 7i
+*/
