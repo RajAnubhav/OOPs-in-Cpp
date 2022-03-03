74fb4c641589b3c34679e27a52ccd378d4c7e050
Anubhav <93657903+RajAnubhav@users.noreply.github.com>
2022-03-03 06:10:24 +0530

    Arrays as Objects in Classes
    We can use arrays as the objects instead of declaring "n" number of objects

--- tutorial25.cpp
+++ tutorial25.cpp
@@ -1,26 +1,49 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Employee{
+class Employee
+{
     int id;
     int salary;
-    public:
-    void setId(void){
-        salary=122;
-        cout<<"Enter Id of the Employee "<<endl;
-        cin>>id;
+
+public:
+    void setId(void)
+    {
+        salary = 122;
+        cout << "Enter Id of the Employee " << endl;
+        cin >> id;
     }
-    void getId(void){
-        cout<<"The Id of this Employee is "<<id<<endl;
+    void getId(void)
+    {
+        cout << "The Id of this Employee is " << id << endl;
     }
 };
-int main(){
-    // Employee harry, rohan, lovish, shruti;
-    // harry.setId();
+int main()
+{
+    // Employee anubhav, rohan, amiyanshu, harshit;
+    // anubhav.setId();
     Employee fb[4];
-    for(int i=0;i<4;i++){
+    // instead of making 4 different objects we can make use of arrays
+    for (int i = 0; i < 4; i++)
+    {
         fb[i].setId();
         fb[i].getId();
     }
     return 0;
 }
+
+// <<<<<---------   OUTPUT   ------------->>>>>>
+/*
+Enter Id of the Employee 
+1
+The Id of this Employee is 1
+Enter Id of the Employee    
+2
+The Id of this Employee is 2
+Enter Id of the Employee
+3
+The Id of this Employee is 3
+Enter Id of the Employee
+4
+The Id of this Employee is 4
+*/
